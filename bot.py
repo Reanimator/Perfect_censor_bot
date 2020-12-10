ec8f250d693e90dcaa3607b74e694f83049c84c9
Reanimator <ageev.mikhail.23@gmail.com>
2020-12-10 18:53:54 +0100

    all ok

--- bot.py
+++ bot.py
@@ -1,11 +1,22 @@
 __author__ = 'Агеев Михаил Михайлович'
 
 import telebot
-from telebot import *
+import re
+# from telebot import *
 
 bot = telebot.TeleBot('1307629489:AAHHuncuzhr__UbKHvds1o737T4p2-eHZMw')
+arr = []
 
 
 @bot.message_handler(content_types=["text"])
 def get_text_messages(message):
-    pass
+    for i in re.split('[^\w]', message.text):
+        if len(i.split('_')) > 1:
+            pass
+        else:
+            arr.append(i)
+    print(arr)
+
+
+
+bot.polling(none_stop=True, interval=0)
